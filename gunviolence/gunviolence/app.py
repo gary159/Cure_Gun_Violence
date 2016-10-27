e160c9a05eee31a6672e51afaabae9af10ba141f
Fenichel <afenichel14@us164760.ad.aol.aoltw.net>
2016-10-27 14:57:58 -0400

    web app

--- gunviolence/gunviolence/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_googlemaps import GoogleMaps
-from ConfigUtil import config
-
-
-
-key=config['GOOGLE_MAPS_KEY']
--- gunviolence/gunviolence/views.py
+++ gunviolence/gunviolence/views.py
@@ -1,13 +1,31 @@
 from gunviolence import app
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from werkzeug.serving import run_simple
+from ConfigUtil import config
 
+key=config['GOOGLE_MAPS_KEY']
+GoogleMaps(app, key=key)
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def main_page():
+	return render_template('main_page.html')
+
+@app.route('/chicago')
+def chicago():
+    # creating a map in the view
+    city_map = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        maptype='ROADMAP',
+        zoom_control=True,
+        markers=[(37.4419, -122.1419)]
+    )
+    
+    return render_template('map.html', city_map=city_map)
+
 
 
 if __name__ == '__main__':
