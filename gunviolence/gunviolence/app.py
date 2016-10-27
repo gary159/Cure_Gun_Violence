2f47cf1008d6966df96a444f5f2e01bdae80624d
Fenichel <afenichel14@us164760.ad.aol.aoltw.net>
2016-10-27 12:27:12 -0400

    setting up run server

---
+++ gunviolence/gunviolence/app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_googlemaps import GoogleMaps
+from ConfigUtil import config
+
+
+
+key=config['GOOGLE_MAPS_KEY']
--- gunviolence/gunviolence/views.py
+++ gunviolence/gunviolence/views.py
@@ -1,5 +1,15 @@
 from gunviolence import app
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+from flask import Flask, render_template
+from werkzeug.serving import run_simple
+
 
 @app.route('/')
 def index():
     return 'Hello World!'
+
+
+if __name__ == '__main__':
+    run_simple('localhost', 5000, app,
+               use_reloader=True, use_debugger=True, use_evalex=True)
